f819fcc2c901060aa1fc35d3e4f27980de61b519
35976 <wygan357@wp.pl>
2024-11-13 16:37:10 +0100

    Update2

--- src/test/java/pl/example/vehicles/TestowanieDzialania.java
+++ src/test/java/pl/example/vehicles/TestowanieDzialania.java
@@ -2,11 +2,34 @@ package pl.example.vehicles;
 
 public class TestowanieDzialania
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws Exception {
         Car car = new Car("Audi", 4);
         Bike bike = new Bike("BMX");
 
 
+        if(car.getMaxSpeed()!=200)
+        {
+            throw new Exception("Bledna wartosc");
+        }
+        if(bike.getMaxSpeed()!=50)
+        {
+            throw new Exception("Bledna wartosc");
+
+        }
+        if(car.getDoors()!=4)
+        {
+            throw new Exception("Bledna wartosc");
+
+        }
+        if(car.getName()!="Audi")
+        {
+            throw new Exception("Bledna wartosc");
+
+        }
+        if(bike.getName()!="BMX")
+        {
+            throw new Exception("Bledna wartosc");
+
+        }
     }
 }
